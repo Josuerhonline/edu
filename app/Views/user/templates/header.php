0d0067fa3b806578c70332e3d010833c5977ea2c
peterdevsv <pedro.antoniohr@gmail.com>
2020-04-09 16:02:56 -0600

    header

--- app/Views/user/templates/header.php
+++ app/Views/user/templates/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
+  <title>Login | EDU</title>
   <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="/user/css/login.css">
   <link rel="stylesheet" href="/fontawesome/css/all.min.css">
