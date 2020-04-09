ebc7e1434d279f81757db3f371e40a9fd9898cfc
peterdevsv <pedro.antoniohr@gmail.com>
2020-04-09 00:36:35 -0600

    mi primer crud

---
+++ app/Controllers/Catalogos/Universidad.php
@@ -0,0 +1,109 @@
+<?php namespace App\Controllers\Catalogos;
+use App\Models\Catalogos\UniversidadModel;
+use App\Controllers\BaseController;
+use \CodeIgniter\Exceptions\PageNotFoundException;
+
+class Universidad extends BaseController {
+
+  public function index(){
+    $universidad = new UniversidadModel();
+
+    $data = [
+      'universidades' => $universidad->asObject()
+      ->select('cof_universidad.*')
+      ->findAll()
+    ];
+    $this->_loadDefaultView( 'Listado de Universidades',$data,'index');
+  }
+
+  public function new(){
+   $user = new UniversidadModel();
+   $persona = new PersonaModel();
+   $roles = new   RolesModel();
+
+   $validation =  \Config\Services::validation();
+   $this->_loadDefaultView('Crear usuario',['validation'=>$validation, 'user'=> new UniversidadModel(),'personas' => $persona->asObject()->findAll(),'rol' => $roles->asObject()->findAll(),'usuarios' => $user->asObject()->findAll()],'new');
+
+ }
+ public function create(){
+  helper("user");
+
+
+  $user = new UniversidadModel();
+
+  if($this->validate('users')){
+   $id = $user->insert([
+    'personaId' =>$this->request->getPost('personaId'),
+    'usuario' =>$this->request->getPost('usuario'),
+    'clave' =>hashClave($this->request->getPost('clave')),
+    'rolId' => $this->request->getPost('rolId'),
+    'estado' => 'EN PROCESO',
+  ]);
+
+   return redirect()->to('/Catalogos/usuario')->with('message', 'usuario creado con éxito.');
+ }
+ return redirect()->back()->withInput();
+}
+public function edit($id = null){
+
+  $user = new UniversidadModel();
+  $persona = new PersonaModel();
+  $roles = new   RolesModel();
+  if ($user->find($id) == null)
+  {
+    throw PageNotFoundException::forPageNotFound();
+  }  
+
+  $validation =  \Config\Services::validation();
+  $this->_loadDefaultView('Actualizar usuario',
+    ['validation'=>$validation,'user'=> $user->asObject()->find($id),'personas' => $persona->asObject()->findAll(),'rol' => $roles->asObject()->findAll(),'usuarios' => $user->asObject()->findAll() ],'edit');
+}
+public function update($id = null){
+  helper("user");
+  $user = new UniversidadModel();
+
+  if ($user->find($id) == null)
+  {
+    throw PageNotFoundException::forPageNotFound();
+  }  
+  if($this->validate('userUpdate')){
+    $user->update($id, [
+      'personaId' =>$this->request->getPost('personaId_editar'),
+      'usuario' =>$this->request->getPost('usuario'),
+      'clave' =>hashClave($this->request->getPost('clave')),
+      'rolId' => $this->request->getPost('rolId_editar'),
+      'estado' =>$this->request->getPost('estado_editar'),
+    ]);
+    return redirect()->to('/Catalogos/usuario')->with('message', 'Usuario editado con éxito.');
+  }
+  return redirect()->back()->withInput();
+}
+
+public function delete($id = null){
+
+  $user = new UniversidadModel();
+
+  if ($user->find($id) == null)
+  {
+    throw PageNotFoundException::forPageNotFound();
+  }  
+
+  $user->delete($id);
+
+  return redirect()->to('/Catalogos/usuario')->with('message', 'usuario eliminado con éxito.');
+  
+}
+
+
+private function _loadDefaultView($title,$data,$view){
+  $dataHeader =[
+    'title' => $title
+  ];
+
+  echo view("dashboard/templates/header",$dataHeader);
+  echo view("Catalogos/universidad/$view",$data);
+  echo view("dashboard/templates/footer");
+}
+
+
+}
---
+++ app/Views/Catalogos/universidad/index.php
@@ -0,0 +1,102 @@
+<?= view("dashboard/edu/menu"); ?>
+</div>
+</div>
+<div class="top_nav">
+  <?= view("dashboard/edu/navbar"); ?>
+  <?= view("dashboard/partials/_session"); ?>
+</div>
+<div class="right_col" role="main">
+  <div class="clearfix"></div>
+  <div class="row">
+    <div class="col-md-12 col-sm-12 ">
+      <div class="x_panel" style="border: 60%">
+        <div class="x_title">
+          <h2>Configuración de Universidades</h2>
+          <div class="clearfix"></div>
+        </div>
+        <div class="x_content" style="background: #fff;border: 1px solid #E1E1E1;padding-top: 10px;border-radius: 10px">
+          <div class="clearfix"></div>
+          <div class="col-md-12">
+            <div class="x_content">
+              <div class="x_content">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="card-box table-responsive">
+                      <a href="/catalogos/usuario/new" title="Crear" class="btn btn-success mb-12" style="margin-left: 19px"><i class="fa fa-plus"></i> Crear</a>
+                      <div class="clearfix"></div>
+                      <table id="tablaUsuarios" class="display table table-bordered responsive nowrap" style="width:100%">
+                       <thead style="background:#2A3F54;">
+                        <tr>
+                          <th style="color: #fff">N°</th>
+                          <th style="color: #fff" >Nombre de Universidad</th>
+                          <th style="color: #fff">Dirección</th>
+                          <th style="color: #fff">Teléfono</th>
+                          <th style="color: #fff">Opciones</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                       <?php $num=0;  foreach ($universidades as $key => $u): ?>
+                      <tr>
+                       <td><?= $num+=1; ?></td>
+                       <td><?= $u->universidad ?></td>
+                       <td><?= $u->direccion ?></td>
+                       <td><?= $u->telefono ?></td>
+                       <td>
+                        <a class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Editar" class="float-right ml-2 btn btn-primary btn-sm" href="/catalogos/universidad/edit/<?= $u->universidadId?>"><i class="fa fa-pencil"></i></a>
+                        <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Borrar" class="float-right btn btn-danger btn-sm ml-2"  onclick="confirmarBorrar(<?= $u->universidadId ?>)"><i class="fa fa-trash"></i></button>
+                      </td>
+                    </tr>
+                  <?php endforeach?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br><br><br><br>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="/js/jquery-3.4.1.slim.min.js"></script>
+<script>
+  function confirmarBorrar(id){
+    swal({   
+      title: "¿Desea eliminar este registro?",   text: "Presione confirmar para eliminar",
+      type: "warning",   
+      showCancelButton: true,   
+      confirmButtonColor: "#DD6B55",   
+      confirmButtonText: "Confirmar",   
+      closeOnConfirm: false 
+    }, function(){   
+      swal({
+        title: "ELIMINADO",
+        text: "Registro eliminado con exito",
+        type: "success",
+      }, 
+      function(){
+       window.location.href = "/Catalogos/universidad/delete/"+id;
+     })
+    });
+  }
+  $(document).ready(function() {
+    $('#tablaUsuarios').DataTable( {
+      "paging":   true,
+      "ordering": true,
+      "info":     true,
+      "language": {
+        "url": "/vendors/datatables.net/españolTables.json"
+      }
+    } );
+  } );
+
+
+</script>
+
+
+
