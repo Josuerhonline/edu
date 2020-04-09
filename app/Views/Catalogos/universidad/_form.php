a7b90ab4fc19a88ee7a38d281d46865e818c17df
Josuerhonline <xjosuerafaelx@gmail.com>
2020-04-09 01:06:28 -0600

    edu-1

--- app/Views/Catalogos/universidad/_form.php
+++ app/Views/Catalogos/universidad/_form.php
@@ -9,6 +9,7 @@
 <div class="right_col" role="main" >
 	<div class="clearfix"></div>
 	<div class="row" >
+		<h1>culo 45</h1>
 		<div class="col-md-12 col-sm-12 " >
 			<div class="x_panel" style="border: 1px solid #e1e1e1;" >
 				<div class="x_title">
@@ -85,27 +86,6 @@
 														<span class="input-group-text" id="basic-addon1"><i class="	fa fa-users" style="color:#2A3F54;width: 20px;height: 24px;
 														"></i></span>
 													</div>
-												</div>
-											</div>
-											<div class="item form-group col-md-12">
-												<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Usuario <span class="required">*</span>
-												</label>
-												<div class="input-group mb-3 col-md-6">
-													<div class="input-group-prepend">
-														<span class="input-group-text" id="basic-addon1"><i class="	fa fa-user" style="color:#2A3F54;width: 20px;height: 24px;
-														"></i></span>
-													</div>
-													<input class="form-control" type="text" id="usuario" name="usuario" value="<?=old('usuario', $user->usuario)?>"/>
-												</div>
-											</div>
-											<div class="item form-group col-md-12">
-												<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Contraseña<span class="required">*</span>
-												</label>
-												<div class="input-group mb-3 col-md-6">
-													<div class="input-group-prepend">
-														<span class="input-group-text" id="basic-addon1">***</span>
-													</div>
-													<input class="form-control" type="password" id="clave" name="clave" value=""/>
 												</div>
 											</div>
 											<button class="btn btn-success" type="submit" style="width: 48%;margin-left: 26%"><i class="fa fa-save"></i> <?=$textButton?></button>
