905ec2847fff460d6c99198602ad2305f53c25ca
Josuerhonline <xjosuerafaelx@gmail.com>
2020-04-09 01:09:59 -0600

    edus

--- app/Views/Catalogos/universidad/new.php
+++ app/Views/Catalogos/universidad/new.php
@@ -1,4 +1,4 @@
 <?= view("dashboard/partials/_form-error"); ?>
 <form action="create" method="POST" enctype="multipart/form-data">
-<?= view("catalogo/universidad/_form",['textButton' => 'Guardar','created' => true]); ?>
-</form>
+<?= view("Catalogos/universidad/_form",['textButton' => 'Guardar','created' => true]); ?>
+</form><!-- FORM -->
