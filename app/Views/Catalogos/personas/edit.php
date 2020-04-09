196e29d4765395480f9b5ac025d4842be70d575d
Josuerhonline <xjosuerafaelx@gmail.com>
2020-04-08 21:00:43 -0600

    edu-1

--- app/Views/Catalogos/personas/edit.php
+++ app/Views/Catalogos/personas/edit.php
@@ -1,4 +1,5 @@
 <?= view("dashboard/partials/_form-error"); ?>
 <form action="/catalogos/personas/update/<?= $personas->personaId ?>" method="POST" enctype="multipart/form-data">
 <?= view("Catalogos/personas/_form",['textButton' => 'Actualizar','created' => false]); ?>
+<h1>culazo</h1>
 </form>
