8f50ac94837959988b2bd08fbcd0764b0fe1baf3
peterdevsv <pedro.antoniohr@gmail.com>
2020-04-12 20:17:19 -0600

    validaciones

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -127,7 +127,7 @@ class Validation
 	public $facultad_errors = [
 		'facultad' => [
 			'required'   => 'Por favor, ingrese el nombre de la Facultad.',
-			'is_unique' => 'La facultad ya existe en la base de datos. Intente nuevamente.',
+			'is_unique'  => 'La facultad ya existe en la base de datos. Intente nuevamente.',
 			'min_length' => 'El nombre de la facultad debe ser mayor a cinco caracteres. Intente nuevamente.',
 			'max_length' => 'El nombre de la facultad debe ser menor a cincuenta caracteres. Intente nuevamente.'
 		],
@@ -147,58 +147,139 @@ class Validation
 	//Validaciones para Universidad
 	public $universidad =[
 		'nombre_universidad' => 'required|is_unique[cof_universidad.universidad]',
-		'direccion' => 'required',
-		'telefono' => 'required|min_length[9]',
+		'direccion' => 'required|min_length[10]|max_length[250]',
+		'telefono'  => 'required|min_length[9]|max_length[9]',
 	];
-	
 	public $universidad_errors = [
 		'nombre_universidad' => [
-			'required'    => 'POR FAVOR, INGRESE EL NOMBRE DE LA UNIVERSIDAD.',
+			'required'  => 'Por favor, ingrese el nombre de la Universidad.',
+			'is_unique' => 'Lo sentimos, la Universidad ya existe en la base de datos. Intente nuevamente.'
 		],
+		'direccion' => [
+			'required'  => 'Por favor, ingrese la dirección de la Universidad.',
+			'min_length' => 'La dirección de la universidad debe ser mayor a diez caracteres. Intente nuevamente.',
+			'max_length' => 'La dirección de la universidad debe ser menor a doscientos cincuenta caracteres. Intente nuevamente.'
+		],
+		'telefono' => [
+			'required'   => 'Por favor, ingrese el Teléfono de la universidad.',
+			'min_length' => 'El Teléfono de la universidad debe ser igual a nueve caracteres. Intente nuevamente.',
+			'max_length' => 'El Teléfono de la universidad debe ser igual a nueve caracteres. Intente nuevamente.'
+		]
 	];
 
 	public $universidadEditar =[
 		'nombre_universidad_editar' => 'required',
-		'direccion_editar' => 'required',
-		'telefono_editar' => 'required|min_length[9]',
+		'direccion_editar'          => 'required|min_length[10]|max_length[250]',
+		'telefono_editar'           => 'required|min_length[9]|max_length[9]',
+	];
+	public $universidadEditar_errors = [
+		'nombre_universidad_editar' => [
+			'required'  => 'Por favor, ingrese el nombre de la Universidad.'
+		],
+		'direccion_editar' => [
+			'required'   => 'Por favor, ingrese la dirección de la Universidad.',
+			'min_length' => 'La dirección de la universidad debe ser mayor a diez caracteres. Intente nuevamente.',
+			'max_length' => 'La dirección de la universidad debe ser menor a doscientos cincuenta caracteres. Intente nuevamente.'
+		],
+		'telefono_editar' => [
+			'required'   => 'Por favor, ingrese el Teléfono de la universidad.',
+			'min_length' => 'El Teléfono de la universidad debe ser igual a nueve caracteres. Intente nuevamente.',
+			'max_length' => 'El Teléfono de la universidad debe ser igual a nueve caracteres. Intente nuevamente.'
+		]
 	];
 
+	//Validaciones para Planes
 	public $plan =[
-		'plan' => 'required|is_unique[cof_planes.nombrePlan]',
+		'plan'        => 'required|is_unique[cof_planes.nombrePlan]',
 		'planAcuerdo' => 'required|min_length[3]',
+	];
+	public $plan_errors = [
+        'plan' => [
+			'required'  => 'Por favor, ingrese el nombre del Plan.',
+			'is_unique' => 'Lo sentimos este Plan ya existe en la base de datos. Intente nuevamente.'
+        ],
+        'planAcuerdo'    => [
+			'required'   => 'Por favor, ingrese el Plan Acuerdo para el Plan.',
+			'min_length' => 'El Plan Acuerdo debe ser mayor a tres caracteres. Intente nuevamente.'
+        ]
 	];
 
 	public $planEditar =[
-		'plan_editar' => 'required|min_length[3]',
+		'plan_editar'        => 'required|min_length[3]',
 		'planAcuerdo_editar' => 'required|min_length[3]',
 	];
+	public $planEditar_errors = [
+        'plan_editar' => [
+			'required'   => 'Por favor, ingrese el nombre del Plan.',
+			'min_length' => 'El Plan debe ser mayor a tres caracteres. Intente nuevamente.'
+        ],
+        'planAcuerdo_editar'    => [
+			'required'   => 'Por favor, ingrese el Plan Acuerdo para el Plan.',
+			'min_length' => 'El Plan Acuerdo debe ser mayor a tres caracteres. Intente nuevamente.'
+        ]
+	];
 
+	//Validaciones para Ciclos
 	public $ciclo =[
-		'ciclo' => 'required|is_unique[cof_aper_ciclo.ciclo]',
-		'anio' => 'required|min_length[3]',
+		'ciclo'               => 'required|min_length[1]|max_length[1]',
+		'anio'                => 'required|min_length[4]|max_length[4]',
 		'nombrePersonalizado' => 'required|is_unique[cof_aper_ciclo.nombrePersonalizado]',
-		'fechaInicio' => 'required',
-		'fechaFin' => 'required',
+		'fechaInicio'         => 'required',
+		'fechaFin'            => 'required',
 	];
 	public $ciclo_errors = [
         'ciclo' => [
-            'required'    => 'Por favor, ingrese el Ciclo.',
+			'required'   => 'Por favor, ingrese el Ciclo.',
+			'min_length' => 'El Ciclo debe tener un caracter (Ej: 1). Intente nuevamente.',
+			'max_length' => 'El Ciclo debe tener un caracter (Ej: 1). Intente nuevamente.'
         ],
-        'anio'    => [
-            'required' => 'Por favor, ingrese el Año para el ciclo.'
+        'anio' => [
+			'required'   => 'Por favor, ingrese el Año para el ciclo.',
+			'min_length' => 'El Año debe tener cuatro caracteres (Ej: 2020). Intente nuevamente.',
+			'max_length' => 'El Año debe tener cuatro caracteres (Ej: 2020). Intente nuevamente.'
         ]
         ,
-        'nombrePersonalizado'    => [
-            'required' => 'Por favor, ingrese el Nombre Personalizado.'
-        ]
+        'nombrePersonalizado' => [
+			'required'  => 'Por favor, ingrese el Nombre Personalizado.',
+			'is_unique' => 'Lo sentimos el Nombre Personalizado ya existe en la base de datos. Intente nuevamente.'
+		],
+		'fechaInicio' 			=> [
+			'required'  => 'Por favor, ingrese la Fecha de Inicio.'
+		],
+		'fechaFin' => [
+			'required'  => 'Por favor, ingrese la Fecha de Finalización.'
+		]
 	];
 	
 	public $cicloEditar =[
-		'ciclo_editar' => 'required',
-		'anio_editar' => 'required|min_length[3]',
+		'ciclo_editar'               => 'required|min_length[1]|max_length[1]',
+		'anio_editar'                => 'required|min_length[4]|max_length[4]',
 		'nombrePersonalizado_editar' => 'required|min_length[3]',
-		'fechaInicio_editar' => 'required',
-		'fechaFin_editar' => 'required',
+		'fechaInicio_editar'         => 'required',
+		'fechaFin_editar'            => 'required',
+	];
+	public $cicloEditar_errors = [
+        'ciclo_editar' => [
+			'required'   => 'Por favor, ingrese el Ciclo.',
+			'min_length' => 'El Ciclo debe tener un caracter (Ej: 1). Intente nuevamente.',
+			'max_length' => 'El Ciclo debe tener un caracter (Ej: 1). Intente nuevamente.'
+        ],
+        'anio_editar' => [
+			'required'   => 'Por favor, ingrese el Año para el ciclo.',
+			'min_length' => 'El Año debe tener cuatro caracteres (Ej: 2020). Intente nuevamente.',
+			'max_length' => 'El Año debe tener cuatro caracteres (Ej: 2020). Intente nuevamente.'
+        ]
+        ,
+        'nombrePersonalizado_editar' => [
+			'required'   => 'Por favor, ingrese el Nombre Personalizado.',
+			'min_length' => 'El nombre personalizado debe ser mayor a tres caracteres. Intente nuevamente.'
+		],
+		'fechaInicio_editar' 			=> [
+			'required'  => 'Por favor, ingrese la Fecha de Inicio.'
+		],
+		'fechaFin_editar' => [
+			'required'  => 'Por favor, ingrese la Fecha de Finalización.'
+		]
 	];
 
 	/**
